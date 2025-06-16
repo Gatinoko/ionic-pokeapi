bc39dcf4416a65d371208576a63711425134623e
Gabriel Tinoco <gabrielslai@gmail.com>
2025-06-16 17:20:23 -0300

    feat: Implemented getAllPokemon and getPokemon functions inside PokeService

--- src/app/services/poke-service.service.ts
+++ src/app/services/poke-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeService {
+  private baseUrl = 'https://pokeapi.co/api/v2/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Returns all available Pokemon
+   */
+  getAllPokemon(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon`)
+  }
+
+  /**
+   * Returns specified Pokemon
+   */
+  getPokemon(name: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon/${name}`)
+  }
 }
