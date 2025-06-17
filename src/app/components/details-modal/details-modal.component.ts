47871d2e6a3ab4273c01cdf17cf9866874c6194a
Gabriel Tinoco <gabrielslai@gmail.com>
2025-06-17 18:33:02 -0300

    feat: Implemented pokeInfo property in DetailsModalComponent

--- src/app/components/details-modal/details-modal.component.ts
+++ src/app/components/details-modal/details-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { OverlayEventDetail } from '@ionic/core/components';
 import { IonicModule, IonModal } from '@ionic/angular';
+import { Pokemon } from 'src/app/types/Pokemon';
 
 @Component({
   standalone: true,
@@ -13,6 +14,25 @@ export class DetailsModalComponent {
   @ViewChild(IonModal) modal!: IonModal;
 
   isModalOpen = false;
+  pokeInfo: Pokemon = {
+    id: 0,
+    name: '',
+    abilities: [],
+    base_experience: 0,
+    height: 0,
+    sprites: {
+      back_default: null,
+      back_female: null,
+      back_shiny: null,
+      back_shiny_female: null,
+      front_default: null,
+      front_female: null,
+      front_shiny: null,
+      front_shiny_female: null,
+    },
+    types: [],
+    weight: 0,
+  };
 
   constructor() {}
 
