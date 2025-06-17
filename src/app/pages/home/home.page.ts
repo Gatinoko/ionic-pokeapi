0c0c5b15fe8f7ffaba83820a40e7325f1f7d6724
Gabriel Tinoco <gabrielslai@gmail.com>
2025-06-17 11:41:17 -0300

    feat: Implemented basic Home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeService } from 'src/app/services/poke-service.service';
 
 @Component({
   standalone: false,
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  public pokemonData: any[] = [];
+
+  constructor(private pokeService: PokeService) {}
+
+  ngOnInit(): void {
+    this.pokeService.getAllPokemon().subscribe((res) => {
+      let data: any[] = [];
+
+      // Assigns PokeAPI response to data array
+      data = res.results;
+
+      // Adds a spriteSrc property to each data array member
+      data.forEach((v, i) => {
+        v.spriteSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          i + 1
+        }.png`;
+      });
+
+      // Assigns transformed data array to pokemonData array
+      this.pokemonData = data;
+    });
+  }
 }
