ccb832ab7269633e07e716fb484bd2d18d43b0f6
Gabriel Tinoco <gabrielslai@gmail.com>
2025-06-19 11:10:46 -0300

    docs: Added comments to FavoritesService

--- src/app/services/favorites-service.service.ts
+++ src/app/services/favorites-service.service.ts
@@ -9,21 +9,33 @@ const FAVORITES_KEY = 'favorites';
 export class FavoritesService {
   constructor() {}
 
+  /**
+   * Gets favorites from local storage
+   */
   getFavorites(): Pokemon[] {
     const data = localStorage.getItem(FAVORITES_KEY);
     return data ? JSON.parse(data) : [];
   }
 
+  /**
+   * Checks whether pokemon is favorited or not
+   */
   isFavorite(pokeName: string): boolean {
     return this.getFavorites().some((item) => item.name === pokeName);
   }
 
+  /**
+   * Adds pokemon to favorites
+   */
   addFavorite(pokemon: Pokemon) {
     const favorites = this.getFavorites();
     favorites.push(pokemon);
     localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
   }
 
+  /**
+   * Removes pokemon from favorites
+   */
   removeFavorite(pokeName: string) {
     const updated = this.getFavorites().filter(
       (item) => item.name !== pokeName
@@ -31,6 +43,9 @@ export class FavoritesService {
     localStorage.setItem(FAVORITES_KEY, JSON.stringify(updated));
   }
 
+  /**
+   * Toggles a pokemon's favorite status
+   */
   toggleFavorite(item: Pokemon) {
     if (this.isFavorite(item.name)) {
       this.removeFavorite(item.name);
